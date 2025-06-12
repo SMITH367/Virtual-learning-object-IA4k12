c419d23468b11437876d78aecd9224c46c298a29
unknown <rodriguezmadridkenny@gmail.com>
2025-06-12 08:58:48 -0500

    Creación de ovaDatasets.js para la unificación de los módulos del OVA de predicción de imágenes

---
+++ src/data/ovaDatasets.js
@@ -0,0 +1,88 @@
+export const ovaDatasets = [
+  {
+    course: "primero",
+    target: "manzana",
+    items: [
+        { "image": "/images/ova/m1.jpeg", "label": "manzana" },
+        { "image": "/images/ova/a1.jpeg", "label": "no-manzana" },
+        { "image": "/images/ova/bp5.jpeg", "label": "no-manzana" },    
+        { "image": "/images/ova/m6.jpeg", "label": "manzana" },
+        { "image": "/images/ova/g5.jpeg", "label": "no-manzana" },
+        { "image": "/images/ova/m3.jpeg", "label": "manzana" },
+        { "image": "/images/ova/m7.jpeg", "label": "manzana" },
+        { "image": "/images/ova/g8.jpeg", "label": "no-manzana" },
+        { "image": "/images/ova/g6.jpeg", "label": "no-manzana" },
+        { "image": "/images/ova/bp3.jpeg", "label": "no-manzana" },
+        { "image": "/images/ova/g7.jpeg", "label": "no-manzana" },
+        { "image": "/images/ova/m2.jpeg", "label": "manzana" },
+        { "image": "/images/ova/a4.jpeg", "label": "no-manzana" },
+        { "image": "/images/ova/bp4.png", "label": "no-manzana" },
+        { "image": "/images/ova/m8.jpeg", "label": "manzana" }
+    ]
+  },
+  {
+    course: "segundo",
+    target: "gato",
+    items: [
+        { "image": "/images/ova/bp1.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/a5.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/g1.jpeg", "label": "gato" },
+        { "image": "/images/ova/m4.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/m2.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/a6.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/bp2.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/g2.jpeg", "label": "gato" },
+        { "image": "/images/ova/g3.jpeg", "label": "gato" },
+        { "image": "/images/ova/a7.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/g4.jpeg", "label": "gato" },
+        { "image": "/images/ova/m5.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/g6.jpeg", "label": "gato" },
+        { "image": "/images/ova/bp3.jpeg", "label": "no-gato" },
+        { "image": "/images/ova/g7.jpeg", "label": "gato" }
+    ]
+  },
+  {
+    course: "tercero",
+    target: "ave",
+    items: [
+        { "image": "/images/ova/a1.jpeg", "label": "ave" },
+        { "image": "/images/ova/bp5.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/g8.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/a6.jpeg", "label": "ave" },
+        { "image": "/images/ova/a7.jpeg", "label": "ave" },
+        { "image": "/images/ova/bp1.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/bp2.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/g1.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/a5.jpeg", "label": "ave" },
+        { "image": "/images/ova/g2.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/g3.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/m4.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/a4.jpeg", "label": "ave" },
+        { "image": "/images/ova/m5.jpeg", "label": "no-ave" },
+        { "image": "/images/ova/m6.jpeg", "label": "no-ave" }
+    ]
+  },
+  {
+    course: "cuarto",
+    target: "botella",
+    items: [
+        { "image": "/images/ova/bp5.jpeg", "label": "botella" },
+        { "image": "/images/ova/g7.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/m1.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/bp3.jpeg", "label": "botella" },
+        { "image": "/images/ova/m2.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/m3.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/a4.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/bp1.jpeg", "label": "botella" },
+        { "image": "/images/ova/a5.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/a7.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/bp2.jpeg", "label": "botella" },
+        { "image": "/images/ova/g1.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/g2.jpeg", "label": "no-botella" },
+        { "image": "/images/ova/bp4.png", "label": "botella" },
+        { "image": "/images/ova/g5.jpeg", "label": "no-botella" }
+    ]
+  }
+  
+  // ...más cursos
+];
